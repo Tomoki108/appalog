770097594400ae8712c51d5d6edc696b7ed24e26
Tomoki108 <shinonome81@gmail.com>
2025-07-23 18:24:23 +0900

    try devcontainer, not work yet

--- services/logger/build.gradle.kts
+++ services/logger/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.0" // Kotlinバージョン
+    kotlin("jvm") version "1.9.0"
     application
 }
 
@@ -7,30 +7,32 @@ group = "com.appalog"
 version = "1.0.0"
 
 repositories {
-    mavenCentral() // 依存関係をMaven Centralから取得
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 dependencies {
-    // Ktorコアライブラリ
     implementation("io.ktor:ktor-server-core:2.3.3")
-    implementation("io.ktor:ktor-server-netty:2.3.3") // Nettyエンジン
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.3") // コンテンツネゴシエーション
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3") // JSONシリアライゼーション
-
-    // ロギング
+    implementation("io.ktor:ktor-server-netty:2.3.3")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
     implementation("ch.qos.logback:logback-classic:1.4.7")
-
-    // テスト
     testImplementation("io.ktor:ktor-server-tests:2.3.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.0")
 }
 
 application {
-    mainClass.set("com.appalog.logger.ApplicationKt") // メインクラス指定
+    mainClass.set("com.appalog.logger.ApplicationKt")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "17" // Java 17互換
+        jvmTarget = "17"
+        freeCompilerArgs = listOf("-Xjsr305=strict")
     }
 }
---
+++ services/logger/src/main/kotlin/com/appalog/logger/Application.kt
@@ -0,0 +1,6 @@
+package com.appalog.logger
+
+fun main() {
+    println("Logger application started")
+    // TODO: Implement logger functionality
+}
